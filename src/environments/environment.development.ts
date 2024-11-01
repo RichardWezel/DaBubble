d808c2058814619aa67e63f21af6271250b5be6b
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-11-01 16:51:51 +0100

    implement firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKl2SLeXKbN_wnJCZPhs1Z4io_bWER57c",
+    authDomain: "da-bubble-f6bb7.firebaseapp.com",
+    projectId: "da-bubble-f6bb7",
+    storageBucket: "da-bubble-f6bb7.firebasestorage.app",
+    messagingSenderId: "485701833167",
+    appId: "1:485701833167:web:4380ffbb377646ae3282ba"
+  },
+};
