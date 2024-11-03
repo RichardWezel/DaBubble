373b7b7d2363e1f5d633c4847d5f30eb61b47dbe
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-03 18:49:59 +0100

    Worked on log-in-card

--- src/app/components/log-in/log-in-card/log-in-card.component.ts
+++ src/app/components/log-in/log-in-card/log-in-card.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgForm, FormsModule } from '@angular/forms';
+import { Auth } from '@angular/fire/auth';
+import { signInWithEmailAndPassword } from '@firebase/auth';
 
 @Component({
   selector: 'app-log-in-card',
@@ -10,6 +12,7 @@ import { NgForm, FormsModule } from '@angular/forms';
   styleUrl: './log-in-card.component.scss'
 })
 export class LogInCardComponent {
+  private auth = inject(Auth);
   focusOnMail = false;
   focusOnPassword = false;
   loginData = {
@@ -19,6 +22,16 @@ export class LogInCardComponent {
 
   checkLogin(ngForm: NgForm) {
     console.log("LoginData: ", this.loginData);
-
+    signInWithEmailAndPassword(this.auth, this.loginData.email, this.loginData.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log('Logged in', user);
+        const uid = user.uid;
+        console.log('UID', uid);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      })
   }
 }
