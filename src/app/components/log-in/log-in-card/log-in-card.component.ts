0804b2bdb886b6da7d442084e9c60e9260222633
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-03 16:56:49 +0100

    Added form attributes

--- src/app/components/log-in/log-in-card/log-in-card.component.ts
+++ src/app/components/log-in/log-in-card/log-in-card.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './log-in-card.component.html',
   styleUrl: './log-in-card.component.scss'
 })
 export class LogInCardComponent {
   focusOnMail = false;
   focusOnPassword = false;
+  loginData = {
+    email: "",
+    password: ""
+  };
+
+  checkLogin(ngForm: NgForm) {
+    console.log("LoginData: ", this.loginData);
+
+  }
 }
