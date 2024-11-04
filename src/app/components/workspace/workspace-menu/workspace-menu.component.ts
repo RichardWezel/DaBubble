e4a7c5efbc87e5040422129be18920135d4862df
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-04 11:51:44 +0100

    workspace-menu head-section hover ready

--- src/app/components/workspace/workspace-menu/workspace-menu.component.ts
+++ src/app/components/workspace/workspace-menu/workspace-menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeadSectionComponent } from './head-section/head-section.component';
 import { ChannelSectionComponent } from './channel-section/channel-section.component';
 import { DmSectionComponent } from './dm-section/dm-section.component';
+import { style } from '@angular/animations';
 
 @Component({
   selector: 'app-workspace-menu',
@@ -12,7 +13,7 @@ import { DmSectionComponent } from './dm-section/dm-section.component';
     DmSectionComponent
   ],
   templateUrl: './workspace-menu.component.html',
-  styleUrl: './workspace-menu.component.scss'
+  styleUrls: ['./workspace-menu.component.scss']
 })
 export class WorkspaceMenuComponent {
 
