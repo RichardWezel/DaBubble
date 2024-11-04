8a9a6152f9b7e42d049592917e6e39b1db8cffc7
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-04 08:52:08 +0100

    added path to styles @workspace-menu-components and added sub-components to html

--- src/app/components/workspace/workspace-menu/workspace-menu.component.ts
+++ src/app/components/workspace/workspace-menu/workspace-menu.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HeadSectionComponent } from './head-section/head-section.component';
+import { ChannelSectionComponent } from './channel-section/channel-section.component';
+import { DmSectionComponent } from './dm-section/dm-section.component';
 
 @Component({
   selector: 'app-workspace-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    HeadSectionComponent,
+    ChannelSectionComponent,
+    DmSectionComponent
+  ],
   templateUrl: './workspace-menu.component.html',
   styleUrl: './workspace-menu.component.scss'
 })
