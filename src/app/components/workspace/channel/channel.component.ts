dbbb4989b1543c3467ea3c97d59639c409d27f88
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-11-05 00:58:27 +0100

    add small optic for channel and thread

--- src/app/components/workspace/channel/channel.component.ts
+++ src/app/components/workspace/channel/channel.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChannelHeadComponent } from "./channel-head/channel-head.component";
+import { InputfieldComponent } from "../../../shared/components/inputfield/inputfield.component";
+import { ChannelMessagesComponent } from "./channel-messages/channel-messages.component";
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [],
+  imports: [ChannelHeadComponent, InputfieldComponent, ChannelMessagesComponent],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
--- src/app/components/workspace/thread/thread.component.ts
+++ src/app/components/workspace/thread/thread.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { InputfieldComponent } from "../../../shared/components/inputfield/inputfield.component";
+import { ThreadMessagesComponent } from "./thread-messages/thread-messages.component";
+import { ThreadHeadComponent } from "./thread-head/thread-head.component";
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [InputfieldComponent, ThreadMessagesComponent, ThreadHeadComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
