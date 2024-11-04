773fff978d80208ed5688766be76540fa0ff740f
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-04 08:20:51 +0100

    added components to workspace.ts and build html workspace

--- src/app/components/workspace/workspace.component.ts
+++ src/app/components/workspace/workspace.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChannelComponent } from './channel/channel.component';
+import { WorkspaceMenuComponent } from './workspace-menu/workspace-menu.component';
+import { ThreadComponent } from './thread/thread.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    WorkspaceMenuComponent,
+    ChannelComponent,
+    ThreadComponent
+  ],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
