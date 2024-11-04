371373c768648293d1a7602d91b3812b9eb763f3
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-11-04 23:36:30 +0100

    add navbar

--- src/app/components/workspace/navbar/navbar.component.ts
+++ src/app/components/workspace/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, inject } from '@angular/core';
+import { SearchComponent } from "./search/search.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  elementRef: ElementRef = inject(ElementRef);
 
+  caretSrc: string = 'assets/icons/user-caret.svg';
+  dropDownOpen: boolean = false;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.querySelector('.current-user').contains(event.target)) {
+      this.dropDownOpen = false;
+    }
+  }
 }
