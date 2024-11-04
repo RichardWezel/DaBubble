82d5f2ff5658b65faacb11880dcfadfb503f6814
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-04 08:26:42 +0100

    added routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WorkspaceComponent },
+    { path: 'login', component: LogInComponent, },
+    { path: 'workspace', component: WorkspaceComponent }
+];
