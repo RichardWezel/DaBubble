123e427098fc279768d103021bfb1300aa636b96
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-05 17:42:37 +0100

    expand animated channel-list

--- src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
+++ src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
@@ -1,14 +1,22 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-channel-section',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './channel-section.component.html',
   styleUrl: './channel-section.component.scss'
 })
 export class ChannelSectionComponent {
 
   channels: string[] = ['Entwicklerteam', 'Marketing', 'Vertrieb', 'Support'];
+
+  // Statusvariable zur Steuerung der Sichtbarkeit
+  isListVisible: boolean = true;
+
+  // Methode zum Umschalten des Status
+  toggleList() {
+    this.isListVisible = !this.isListVisible;
+  }
 }
