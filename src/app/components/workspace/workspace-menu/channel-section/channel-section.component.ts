38bfca28b409e9c16f6ab76aab2f3e5fc7c17566
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-05 17:22:54 +0100

    workspace-menu channel section html and scss basic ready with binding

--- src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
+++ src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-channel-section',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './channel-section.component.html',
   styleUrl: './channel-section.component.scss'
 })
 export class ChannelSectionComponent {
 
+  channels: string[] = ['Entwicklerteam', 'Marketing', 'Vertrieb', 'Support'];
 }
