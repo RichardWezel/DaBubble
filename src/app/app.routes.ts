eae3f97c344877beaca9de4061e404a56062f53c
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-08 13:38:15 +0100

    Added paths and added sign-in-card component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { LogInComponent } from './components/log-in/log-in/log-in.component';
+import { SignInComponent } from './components/log-in/sign-in/sign-in.component';
+import { ResetPasswordComponent } from './components/log-in/reset-password/reset-password.component';
 
 export const routes: Routes = [
     { path: '', component: WorkspaceComponent },
     { path: 'login', component: LogInComponent, },
+    { path: 'signin', component: SignInComponent },
+    { path: 'resetpassword', component: ResetPasswordComponent},
     { path: 'workspace', component: WorkspaceComponent }
 ];
---
+++ src/app/components/log-in/sign-in/sign-in-card/sign-in-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-card.component.html',
+  styleUrl: './sign-in-card.component.scss'
+})
+export class SignInCardComponent {
+
+}
