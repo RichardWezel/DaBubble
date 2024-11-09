2102aabb5b0226fafaf16bdc3d047960a64e11f0
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-09 17:46:27 +0100

    Worked on the sign-in-form

--- src/app/components/log-in/sign-in/sign-in-card/sign-in-card.component.ts
+++ src/app/components/log-in/sign-in/sign-in-card/sign-in-card.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../../shared/components/log-in/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-card',
   standalone: true,
-  imports: [ CardComponent ],
+  imports: [ CardComponent, FormsModule ],
   templateUrl: './sign-in-card.component.html',
   styleUrl: './sign-in-card.component.scss'
 })
 export class SignInCardComponent {
-
+  signInData = {
+    name: "",
+    email: "",
+    password: ""
+  };
 }
--- src/app/components/log-in/sign-in/sign-in.component.ts
+++ src/app/components/log-in/sign-in/sign-in.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from "../../../shared/components/log-in/background/background.component";
 import { SignInCardComponent } from "./sign-in-card/sign-in-card.component";
+import { ChooseAvatarCardComponent } from "./choose-avatar-card/choose-avatar-card.component";
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [BackgroundComponent, SignInCardComponent],
+  imports: [BackgroundComponent, SignInCardComponent, ChooseAvatarCardComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
