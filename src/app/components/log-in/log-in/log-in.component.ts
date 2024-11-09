056880e6d61e9c053005944c89ea5a23908dc517
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-09 23:16:18 +0100

    Styled send-email-card.component

--- src/app/components/log-in/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in/log-in.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LogInCardComponent } from './log-in-card/log-in-card.component';
 import { BackgroundWithSignInComponent } from "../../../shared/components/log-in/background-with-sign-in/background-with-sign-in.component";
+import { SendEmailCardComponent } from "./send-email-card/send-email-card.component";
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [LogInCardComponent, BackgroundWithSignInComponent],
+  imports: [LogInCardComponent, BackgroundWithSignInComponent, SendEmailCardComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
--- src/app/components/log-in/log-in/send-email-card/send-email-card.component.ts
+++ src/app/components/log-in/log-in/send-email-card/send-email-card.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../../../shared/components/log-in/card/card.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-send-email-card',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, FormsModule],
   templateUrl: './send-email-card.component.html',
   styleUrl: './send-email-card.component.scss'
 })
 export class SendEmailCardComponent {
-
+  mailData: string = '';
 }
