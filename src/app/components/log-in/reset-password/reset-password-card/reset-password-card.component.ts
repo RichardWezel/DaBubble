04609905b413727705c5b32a4eec00a82ec706e4
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-10 00:14:17 +0100

    Worked on the reset password card

--- src/app/components/log-in/reset-password/reset-password-card/reset-password-card.component.ts
+++ src/app/components/log-in/reset-password/reset-password-card/reset-password-card.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../../../shared/components/log-in/card/card.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password-card',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent,FormsModule],
   templateUrl: './reset-password-card.component.html',
   styleUrl: './reset-password-card.component.scss'
 })
 export class ResetPasswordCardComponent {
+  passwordData: string = '';
 
 }
