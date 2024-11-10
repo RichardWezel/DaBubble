841f064d4dbc2afd60c740452c60191d41062d39
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-10 10:50:25 +0100

    added paths in routes.ts of choose-avatar,imprint,privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LogInComponent } from './components/log-in/log-in/log-in.component';
 import { SignInComponent } from './components/log-in/sign-in/sign-in.component';
 import { ResetPasswordComponent } from './components/log-in/reset-password/reset-password.component';
 import { ChooseAvatarCardComponent } from './components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: WorkspaceComponent },
@@ -12,4 +14,6 @@ export const routes: Routes = [
     { path: 'resetpassword', component: ResetPasswordComponent},
     { path: 'workspace', component: WorkspaceComponent },
     { path: 'choose-avatar', component: ChooseAvatarCardComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
