89099213cc5456fbf6c0e3af6af02aba3ec615a2
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-11-10 03:36:28 +0100

    start channel-messages

--- src/app/components/workspace/channel/channel-messages/channel-messages.component.ts
+++ src/app/components/workspace/channel/channel-messages/channel-messages.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { ChannelHeadComponent } from '../channel-head/channel-head.component';
 
 @Component({
   selector: 'app-channel-messages',
   standalone: true,
-  imports: [],
+  imports: [ChannelHeadComponent],
   templateUrl: './channel-messages.component.html',
   styleUrl: './channel-messages.component.scss'
 })
-export class ChannelMessagesComponent {
+export class ChannelMessagesComponent extends ChannelHeadComponent {
 
+  constructor() {
+    super();
+  }
+
+  getPosts() {
+    let getPosts = this.storage.channel.find(channel => channel.id === this.storage.currentUser.currentChannel)?.posts;
+    if (getPosts) return getPosts;
+    else return [];
+  }
 }
