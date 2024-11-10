389173367ec08a53840fe545ec8b153da972fcfd
Ogulcan Erdag <ogulcanerdag@gmail.com>
2024-11-10 21:38:57 +0100

    small size environment

--- src/app/components/log-in/log-in/log-in-card/log-in-card.component.ts
+++ src/app/components/log-in/log-in/log-in-card/log-in-card.component.ts
@@ -3,46 +3,45 @@ import { NgForm, FormsModule } from '@angular/forms';
 import { Auth } from '@angular/fire/auth';
 import { signInWithEmailAndPassword } from '@firebase/auth';
 import { CardComponent } from '../../../../shared/components/log-in/card/card.component';
-import { Router } from '@angular/router'; // Import Router
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-log-in-card',
   standalone: true,
-  imports: [FormsModule, CardComponent],
+  imports: [FormsModule, CardComponent, CommonModule],
   templateUrl: './log-in-card.component.html',
   styleUrl: './log-in-card.component.scss'
 })
 export class LogInCardComponent {
   private auth = inject(Auth);
-  private router = inject(Router); // Inject Router
+  private router = inject(Router);
   loginData = {
     email: "",
     password: ""
   };
   @Output() login = new EventEmitter<boolean>();
 
+  
   goToForgetPassword() {
     this.login.emit(false);
   }
 
+  
   checkLogin(ngForm: NgForm) {
     console.log("LoginData: ", this.loginData);
     signInWithEmailAndPassword(this.auth, this.loginData.email, this.loginData.password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log('Logged in', user);
-        const uid = user.uid;
-        console.log('UID', uid);
-        this.router.navigate(['/workspace']); // Navigate to the chat page
+        console.log('Eingeloggt', user);
+        this.router.navigate(['/workspace']);
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        console.error("Fehler beim Einloggen: ", error.message);
       });
   }
 
-  // Guest login function
   guestLogin() {
-    this.router.navigate(['/workspace']); // Navigate to chat page for guest login
+    this.router.navigate(['/workspace']);
   }
 }
