95fc115d0edea5f186e5e4c4bdb6dee40635ce7d
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-10 16:34:01 +0100

    Styled the chose-avatar-card

--- src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
+++ src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../../../shared/components/log-in/card/card.component';
 
 @Component({
   selector: 'app-choose-avatar-card',
   standalone: true,
-  imports: [],
+  imports: [ CardComponent ],
   templateUrl: './choose-avatar-card.component.html',
   styleUrl: './choose-avatar-card.component.scss'
 })
 export class ChooseAvatarCardComponent {
-
+  numbersOfProfilePictures = [1, 2, 3, 4, 5, 6];
 }
