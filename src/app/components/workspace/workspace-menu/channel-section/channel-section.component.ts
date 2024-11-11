db6e17ea686a9f0ed33cc36657dd8b6e3ef346b9
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-11 15:36:53 +0100

    deleted unnessessary comments

--- src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
+++ src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
@@ -10,16 +10,11 @@ import { FirebaseStorageService } from '../../../../shared/services/firebase-sto
   styleUrl: './channel-section.component.scss'
 })
 export class ChannelSectionComponent {
-  // Injecting the Firebase storage service
+
   storage = inject(FirebaseStorageService);
 
-  // An array for storing channel names
-  channels: string[] = ['Entwicklerteam', 'Marketing', 'Vertrieb', 'Support'];
-
-  // Status variable to control visibility of the channel list
   isListVisible: boolean = true;
 
-  // Method to toggle the visibility of the list
   toggleList() {
     this.isListVisible = !this.isListVisible;
   }
