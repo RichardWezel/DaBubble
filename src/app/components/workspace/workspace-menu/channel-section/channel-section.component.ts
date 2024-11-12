150c13232b7e0f772b7915aa09ada68e1b4728b8
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-11-12 21:51:37 +0100

    added active style to channel-section

--- src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
+++ src/app/components/workspace/workspace-menu/channel-section/channel-section.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
 import { FormsModule } from '@angular/forms';
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-channel-section',
   standalone: true,
-  imports: [NgFor, NgIf, FormsModule], 
+  imports: [NgFor, NgIf, FormsModule, NgClass], 
   templateUrl: './channel-section.component.html',
   styleUrl: './channel-section.component.scss'
 })
