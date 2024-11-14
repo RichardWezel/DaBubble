d387ba7e433b9c2f96548adf07c25fad733f9f0b
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-11-14 02:51:16 +0100

    started thread messages container

--- src/app/components/workspace/thread/thread-head/thread-head.component.ts
+++ src/app/components/workspace/thread/thread-head/thread-head.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
+import { ChannelHeadComponent } from '../../channel/channel-head/channel-head.component';
 
 @Component({
   selector: 'app-thread-head',
@@ -8,12 +8,14 @@ import { FirebaseStorageService } from '../../../../shared/services/firebase-sto
   templateUrl: './thread-head.component.html',
   styleUrl: './thread-head.component.scss'
 })
-export class ThreadHeadComponent {
-  storage = inject(FirebaseStorageService);
+export class ThreadHeadComponent extends ChannelHeadComponent {
 
-  constructor() { }
 
-  channelName() {
+  constructor() {
+    super();
+  }
+
+  channelNameThread() {
     let currentChannel = this.storage.currentUser.currentChannelName;
     if (currentChannel?.startsWith('#')) return currentChannel.replace('#', '# ');
     else return '@ ' + currentChannel;
--- src/app/components/workspace/thread/thread-messages/thread-messages.component.ts
+++ src/app/components/workspace/thread/thread-messages/thread-messages.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ChannelMessagesComponent } from '../../channel/channel-messages/channel-messages.component';
+import { DateSeparatorComponent } from '../../../../shared/components/date-separator/date-separator.component';
+import { MessageComponent } from '../../../../shared/components/message/message.component';
 
 @Component({
   selector: 'app-thread-messages',
   standalone: true,
-  imports: [],
+  imports: [DateSeparatorComponent, MessageComponent],
   templateUrl: './thread-messages.component.html',
   styleUrl: './thread-messages.component.scss'
 })
-export class ThreadMessagesComponent {
+export class ThreadMessagesComponent extends ChannelMessagesComponent {
 
+
+  constructor() {
+    super();
+  }
+
+  getThreadOfPost() {
+    let posts = this.storage.channel.find(channel => channel.id === this.storage.currentUser.currentChannel)?.posts;
+    let post = posts?.find(post => post.id === this.storage.currentUser.postId);
+    if (post) return post.threadMsg;
+    else return [];
+  }
+
+  getThreadOfDm() {
+    let posts = this.storage.currentUser.dm.find(dm => dm.id === this.storage.currentUser.currentChannel)?.posts;
+    let post = posts?.find(post => post.id === this.storage.currentUser.postId);
+    return post ? post.threadMsg : [];
+  }
 }
