7d3250212684e42eb637edeb76df5f651130ff6e
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-15 11:27:06 +0100

    Deleted import ChoseChooseAvatarCardComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { LogInComponent } from './components/log-in/log-in/log-in.component';
 import { SignInComponent } from './components/log-in/sign-in/sign-in.component';
 import { ResetPasswordComponent } from './components/log-in/reset-password/reset-password.component';
-import { ChooseAvatarCardComponent } from './components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component';
 import { ImprintComponent } from './components/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
