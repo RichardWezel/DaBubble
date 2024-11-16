fd0bd6e205892b94c0b62f663671cec0e32021c6
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-16 13:31:04 +0100

    You can chose a picture from the file explorer

--- src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
+++ src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
@@ -1,19 +1,48 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../../shared/components/log-in/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-choose-avatar-card',
   standalone: true,
-  imports: [ CardComponent ],
+  imports: [ CardComponent, CommonModule ],
   templateUrl: './choose-avatar-card.component.html',
   styleUrl: './choose-avatar-card.component.scss'
 })
 export class ChooseAvatarCardComponent {
-  pictures = ['profile-pictures/profile-1.png', 'profile-pictures/profile-2.png', 'profile-pictures/profile-3.png', 'profile-pictures/profile-4.png', 'profile-pictures/profile-5.png', 'profile-pictures/profile-6.png'];
-  currentProfilePicture = 'profile-basic.png';
+  pictures = ['assets/img/profile-pictures/profile-1.png', 'assets/img/profile-pictures/profile-2.png', 'assets/img/profile-pictures/profile-3.png', 'assets/img/profile-pictures/profile-4.png', 'assets/img/profile-pictures/profile-5.png', 'assets/img/profile-pictures/profile-6.png'];
+  currentProfilePicture = 'assets/img/profile-basic.png';
   avatarSelected = false;
 
   chosePicture(path: string) {
     this.currentProfilePicture = path;
+    this.avatarSelected = true;
   }
+
+  openFileExplorer(fileInput: HTMLInputElement) {
+    fileInput.click();
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      const file = input.files[0];
+      console.log('file ', file);
+      const reader = new FileReader();
+  
+      reader.onload = () => {
+        if (reader.result) {
+          this.currentProfilePicture = reader.result as string;
+          this.avatarSelected = true;
+        }
+      };
+  
+      reader.onerror = () => {
+        console.error('Fehler beim Lesen der Datei');
+      };
+  
+      reader.readAsDataURL(file);
+    }
+  }
+
 }
