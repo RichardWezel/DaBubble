232827e13f2862270eda8584e6306f222ca8f82a
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-11-21 21:06:24 +0100

    Sign in function works

--- src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
+++ src/app/components/log-in/sign-in/choose-avatar-card/choose-avatar-card.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { SignInService } from '../../../../shared/services/sign-in.service';
 import { FormsModule } from '@angular/forms';
 import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
+import { Auth, createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
+import { NavigationService } from '../../../../shared/services/navigation.service';
 
 @Component({
   selector: 'app-choose-avatar-card',
@@ -14,10 +16,12 @@ import { FirebaseStorageService } from '../../../../shared/services/firebase-sto
 })
 export class ChooseAvatarCardComponent {
   pictures = ['assets/img/profile-pictures/profile-1.png', 'assets/img/profile-pictures/profile-2.png', 'assets/img/profile-pictures/profile-3.png', 'assets/img/profile-pictures/profile-4.png', 'assets/img/profile-pictures/profile-5.png', 'assets/img/profile-pictures/profile-6.png'];
-  currentProfilePicture = 'assets/img/profile-basic.png';
-  avatarSelected = false;
   signInService: SignInService = inject(SignInService);
+  currentProfilePicture = this.signInService.signInData.img || 'assets/img/profile-basic.png';
+  avatarSelected = this.signInService.signInData.img != '';
   storage: FirebaseStorageService = inject(FirebaseStorageService);
+  private auth: Auth = inject(Auth);
+  navigationService: NavigationService = inject(NavigationService);
   @Output() generateAccount = new EventEmitter<boolean>();
 
 
@@ -78,15 +82,74 @@ export class ChooseAvatarCardComponent {
 
 
   /**
-   * Adds a new user to the user collection in the Firebase storage.
+   * Adds a new user in the Firebase Authentication and gets the uid from the authentication as authUid back.
+   * A new user gets crated in the Firestore Database with the authUid and the insereted user data.
+   * When the authentication and the new user are created the signinData gets cleared and the user gets routed to the login.
    */
   setNewUser() {
-    console.log('Data', this.signInService.signInData);
-    const userData = {
+    this.setUserInFirebaseAuthentication()
+      .then((authUid) => {
+        const userData = this.getSignInData();
+        this.storage.addUser(authUid, userData);
+        this.clearSignInServiceData();
+        this.goBackToLogin();
+      })
+      .catch((error) => {
+        throw error;
+      })
+  }
+
+  
+  /**
+   * This method creates a new user in the authentication from Firebase with the inserted email and password.
+   * The user uid gets returned as authUid to create a new user document with this id.
+   * @returns - AuthUid to create the id for a new user document.
+   */
+  async setUserInFirebaseAuthentication(): Promise<string> {
+    const auth = getAuth();
+    const email = this.signInService.signInData.email;
+    const password = this.signInService.signInData.password;
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const authUid = userCredential.user.uid;
+      return authUid;
+    } catch (error ) {
+      throw error;
+    }
+  }
+
+
+  /**
+   * Returns the name, email and avatar from the signInData from the signInService to create a new user document.
+   * @returns - An object with the insereted name, email and selected avatar.
+   */
+  getSignInData(): any {
+    return {
       name: this.signInService.signInData.name,
       email: this.signInService.signInData.email,
       avatar: this.signInService.signInData.img,
+    }
+  }
+
+
+  /**
+   * Clears the variables in the signInData object.
+   */
+  clearSignInServiceData(): void {
+    this.signInService.signInData = {
+      name: "",
+      email: "",
+      password: "",
+      checkboxChecked: false,
+      img: ""
     };
-    this.storage.addUser('abcdefghi111', userData);
+  }
+
+
+  /**
+   * Routes the user to the login component.
+   */
+  goBackToLogin() {
+    this.navigationService.navigateTo('login');
   }
 }
