b80379de705e22e1b961c825effad7c5e5d4c636
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-12-11 22:40:23 +0100

    style search component without any function

--- src/app/components/workspace/channel/channel.component.ts
+++ src/app/components/workspace/channel/channel.component.ts
@@ -3,11 +3,12 @@ import { ChannelHeadComponent } from "./channel-head/channel-head.component";
 import { InputfieldComponent } from "../../../shared/components/inputfield/inputfield.component";
 import { ChannelMessagesComponent } from "./channel-messages/channel-messages.component";
 import { FirebaseStorageService } from '../../../shared/services/firebase-storage.service';
+import { SearchComponent } from '../navbar/search/search.component';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [ChannelHeadComponent, InputfieldComponent, ChannelMessagesComponent],
+  imports: [ChannelHeadComponent, InputfieldComponent, ChannelMessagesComponent, SearchComponent],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
