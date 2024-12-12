9101ce25bff971fdc1789b62296f1103ec2a271a
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-12-12 21:51:48 +0100

    Refactoring log-in component

--- src/app/components/log-in/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in/log-in.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { LogInCardComponent } from './log-in-card/log-in-card.component';
 import { BackgroundWithSignInComponent } from "../../../shared/components/log-in/background-with-sign-in/background-with-sign-in.component";
 import { SendEmailCardComponent } from "./send-email-card/send-email-card.component";
-import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthorService } from '../../../shared/services/author.service';
 
 
 @Component({
@@ -17,17 +15,13 @@ import { AuthorService } from '../../../shared/services/author.service';
 export class LogInComponent {
 
   login: boolean = true;
-  password: boolean = false;
   showLoading: boolean = true;
 
-  constructor(private route: ActivatedRoute, private authorservice: AuthorService, private router: Router) { }
-
+  
+  /**
+   * When the component gets initialized, the start overlay is shown for 3 seconds.
+   */
   ngOnInit() {
-    const urlSegments = this.route.snapshot.url.map(segment => segment.path);
-    if (urlSegments.includes('password-main') && urlSegments.includes('reset-password')) {
-      this.password = true;
-    }
-
     setTimeout(() => {
       this.showLoading = false;
     }, 3000);
