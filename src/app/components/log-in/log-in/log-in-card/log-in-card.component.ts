5824d9db12241d69e1a857230c277fb022f1f8b2
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-12-12 23:45:12 +0100

    Refactoring log-in-card component

--- src/app/components/log-in/log-in/log-in-card/log-in-card.component.ts
+++ src/app/components/log-in/log-in/log-in-card/log-in-card.component.ts
@@ -5,12 +5,11 @@ import { CardComponent } from '../../../../shared/components/log-in/card/card.co
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Auth } from '@angular/fire/auth';
-import { Firestore, doc } from '@angular/fire/firestore';
+// import { Firestore, doc } from '@angular/fire/firestore';
 import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
-import { PostInterface } from '../../../../shared/interfaces/post.interface';
+// import { PostInterface } from '../../../../shared/interfaces/post.interface';
 import { FirebaseAuthService } from '../../../../shared/services/firebase-auth.service';
 import { NavigationService } from '../../../../shared/services/navigation.service';
-import { ResetPasswordCardComponent } from '../../reset-password/reset-password-card/reset-password-card.component';
 
 @Component({
   selector: 'app-log-in-card',
@@ -22,87 +21,82 @@ import { ResetPasswordCardComponent } from '../../reset-password/reset-password-
 export class LogInCardComponent {
   private auth = inject(Auth);
   private router = inject(Router);
-  private firestore = inject(Firestore);
+  // private firestore = inject(Firestore);
   protected storage = inject(FirebaseStorageService);
   navigationService: NavigationService = inject(NavigationService);
   authService = inject(FirebaseAuthService);
-  @Input() post: PostInterface = { text: '', author: '', timestamp: 0, thread: false, id: '' };
-  loginData = {
+
+  loginData: { email: string; password: string } = {
     email: '',
     password: '',
   };
-
   passwordVisible: boolean = false;
   errorMessage: string = '';
   isLoading: boolean = false;
 
+  // @Input() post: PostInterface = { text: '', author: '', timestamp: 0, thread: false, id: '' };
   @Output() login = new EventEmitter<boolean>();
-  @Output() newAccount = new EventEmitter<boolean>();
 
+
+  /**
+   * Shows the send-email-card to insert the mail for resetting the password.
+   */
   goToSendMail() {
     this.login.emit(false);
-    this.newAccount.emit(false);
   }
 
+
+  /**
+   * When you click on the lock image you can show or hide the password.
+   */
   togglePasswordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
   }
 
-  checkLogin(ngForm: NgForm) {
-    if (ngForm.invalid) {
-      this.errorMessage = "Bitte füllen Sie alle Felder korrekt aus.";
-      return;
-    }
-    this.errorMessage = ''; // Fehlermeldung zurücksetzen
 
+  /**
+   * 
+   * @param ngForm 
+   */
+  checkLogin(ngForm: NgForm) {
+    this.errorMessage = ''; // Reset error message
     console.log("Login gestartet...");
     signInWithEmailAndPassword(this.auth, this.loginData.email, this.loginData.password)
       .then(async (userCredential) => {
         const user = userCredential.user;
-
-        // Überprüfen, ob die E-Mail-Adresse verifiziert ist
-        if (!user.emailVerified) {
-          this.errorMessage = "Ihre E-Mail-Adresse ist noch nicht verifiziert. Bitte überprüfen Sie Ihren Posteingang.";
-          return; // Stoppt die weitere Verarbeitung
-        }
-
         console.log("Benutzer eingeloggt:", user);
 
-        // Speichern der Auth-UID
+        // Saving the Auth-UID
         sessionStorage.setItem("authUid", user.uid);
         this.storage.authUid = user.uid;
 
-        // Benutzerinformationen laden
+        // Loading the user information
         this.authService.getCurrentUser();
         this.storage.getCurrentUserChannelCollection();
 
         console.log("Benutzerkanäle geladen:", this.storage.CurrentUserChannel);
 
-        // Benutzerstatus auf "online" setzen
+        // Set user status to "online"
         await this.authService.setCurrentUserOnline(user.uid);
 
         this.router.navigate(['/workspace']);
       })
-      
       .catch((error) => {
+        console.log('Error code', error.code);
         switch (error.code) {
-          case 'auth/user-not-found':
-            this.errorMessage = "Anmeldung fehlgeschlagen! Überprüfen Sie Ihre Anmeldedaten.";
-            break;
-          case 'auth/wrong-password':
-            this.errorMessage = "Das eingegebene Passwort ist falsch. Bitte versuchen Sie es erneut.";
+          case 'auth/invalid-credential':
+            this.errorMessage = "Die Anmeldeinformationen sind ungültig. Überprüfen Sie Ihre Anmeldedaten.";
             break;
           case 'auth/too-many-requests':
             this.errorMessage = "Zu viele Anmeldeversuche. Bitte versuchen Sie es später erneut.";
             break;
           default:
-            this.errorMessage = "Es gibt kein Konto mit dieser E-Mail-Adresse. Bitte registrieren Sie sich zuerst.";
+            this.errorMessage = "Es gibt ein Problem bei der Anmeldung. Bitte versuchen Sie es erneut.";
         }
       });
-      
   }
 
-  getGoogleLoginErrorMessage(errorCode: string): string {
+/*   getGoogleLoginErrorMessage(errorCode: string): string {
     switch (errorCode) {
       case 'auth/popup-closed-by-user':
         return 'Das Anmelde-Popup wurde geschlossen, bevor die Anmeldung abgeschlossen werden konnte.';
@@ -111,9 +105,9 @@ export class LogInCardComponent {
       default:
         return 'Fehler bei der Anmeldung mit Google. Bitte versuche es später erneut.';
     }
-  }
+  } */
 
-  resetPassword() {
+/*   resetPassword() {
     if (!this.loginData.email) {
       alert('Bitte geben Sie Ihre E-Mail-Adresse ein, um das Passwort zurückzusetzen.');
       return;
@@ -132,6 +126,6 @@ export class LogInCardComponent {
   validateEmail(email: string): boolean {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  }
+  } */
 
 }
