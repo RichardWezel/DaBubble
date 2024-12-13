2e84838e3d655adc9017d1251ba927c35f0c8991
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-12-13 13:54:54 +0100

    Added removed function, refactoring and comments

--- src/app/components/log-in/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in/log-in.component.ts
@@ -3,6 +3,7 @@ import { LogInCardComponent } from './log-in-card/log-in-card.component';
 import { BackgroundWithSignInComponent } from "../../../shared/components/log-in/background-with-sign-in/background-with-sign-in.component";
 import { SendEmailCardComponent } from "./send-email-card/send-email-card.component";
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -16,14 +17,35 @@ export class LogInComponent {
 
   login: boolean = true;
   showLoading: boolean = true;
+  password: boolean = false;
+
+
+  /**
+   * The constructor injects the 'ActivatedRoute' service to access information about the current route.
+   * @param route - The Angular service used to access route-related information.
+   */
+  constructor(private route: ActivatedRoute) { }
 
   
   /**
-   * When the component gets initialized, the start overlay is shown for 3 seconds.
+   * When the component gets initialized, the passthe start overlay is shown for 3 seconds.
    */
   ngOnInit() {
+    this.isPasswordResetRoute();
     setTimeout(() => {
       this.showLoading = false;
     }, 3000);
+  }
+
+
+  /**
+   * Checks if the current route URL contains sepcific segments.
+   * If both segments are prsent, the 'password' property is set to true.
+   */
+  isPasswordResetRoute() {
+    const urlSegments = this.route.snapshot.url.map(segment => segment.path);
+    if (urlSegments.includes('password-main') && urlSegments.includes('reset-password')) {
+      this.password = true;
+    }
   }
 }
