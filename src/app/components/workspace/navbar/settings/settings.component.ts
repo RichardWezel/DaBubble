1d576587ab4c2852431c87a001b8b577e5c7e175
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-12-29 22:39:04 +0100

    mobile style of settings

--- src/app/components/workspace/navbar/settings/settings.component.ts
+++ src/app/components/workspace/navbar/settings/settings.component.ts
@@ -1,12 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, } from '@angular/core';
 import { FirebaseAuthService } from '../../../../shared/services/firebase-auth.service';
 import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
 import { OpenCloseDialogService } from '../../../../shared/services/open-close-dialog.service';
+import { NgIf } from '@angular/common';
+import { SetMobileViewService } from '../../../../shared/services/set-mobile-view.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
@@ -15,12 +18,26 @@ export class SettingsComponent {
   storage = inject(FirebaseStorageService);
   authService = inject(FirebaseAuthService);
   openCloseDialogService = inject(OpenCloseDialogService);
+  isLargeScreen: boolean = false;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+   constructor(private viewService: SetMobileViewService) {}
 
   openUserProfile() {
     this.openCloseDialogService.changeProfileId(this.storage.currentUser.id!);
     this.openCloseDialogService.open('userProfile');
+  }
+
+  ngOnInit(): void {
+
+    const screenSub = this.viewService.isLargeScreen$.subscribe(isLarge => {
+      this.isLargeScreen = isLarge;
+    });
+    this.subscriptions.add(screenSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
