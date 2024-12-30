78ea1006e59233e8e0043673e248c7a526b8746f
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-12-30 18:12:38 +0100

    style settings

--- src/app/components/workspace/navbar/settings/settings.component.ts
+++ src/app/components/workspace/navbar/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, } from '@angular/core';
+import { Component, inject, Renderer2, ElementRef, HostBinding, Input, Output, EventEmitter, OnInit, OnDestroy} from '@angular/core';
 import { FirebaseAuthService } from '../../../../shared/services/firebase-auth.service';
 import { FirebaseStorageService } from '../../../../shared/services/firebase-storage.service';
 import { OpenCloseDialogService } from '../../../../shared/services/open-close-dialog.service';
@@ -13,7 +13,14 @@ import { Subscription } from 'rxjs';
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
-export class SettingsComponent {
+export class SettingsComponent  implements OnInit, OnDestroy{
+
+  @Input() open: boolean = false; // Neue Input-Eigenschaft
+  @Output() close = new EventEmitter<void>(); // Neue Output-Eigenschaft
+
+  @HostBinding('class.open') get isOpen() {
+    return this.open;
+  }
 
   storage = inject(FirebaseStorageService);
   authService = inject(FirebaseAuthService);
@@ -21,11 +28,17 @@ export class SettingsComponent {
   isLargeScreen: boolean = false;
   private subscriptions: Subscription = new Subscription();
 
-   constructor(private viewService: SetMobileViewService) {}
+   constructor(
+    private viewService: SetMobileViewService,
+    private renderer: Renderer2, // For manipulating classes
+    private el: ElementRef // To access DOM elements
+   ) {}
 
   openUserProfile() {
     this.openCloseDialogService.changeProfileId(this.storage.currentUser.id!);
     this.openCloseDialogService.open('userProfile');
+    this.close.emit(); // Optional: Dropdown nach dem Öffnen schließen
+    console.log('User profile opened and dropdown closed.');
   }
 
   ngOnInit(): void {
@@ -38,6 +51,12 @@ export class SettingsComponent {
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+  }
+
+  // Methode zum Schließen des Dropdowns
+  toggleDropdown(): void {
+    this.close.emit(); // Emitiert das `close`-Ereignis
+    console.log('Dropdown closed via toggle.');
   }
 
 }
