f44eda13c6c4cdcd229043efb6bab5ba6524a7ef
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2025-01-03 15:54:44 +0100

    Generated add-member-to-channel-dialog

---
+++ src/app/components/workspace/workspace-menu/channel-section/add-member-to-channel-dialog/add-member-to-channel-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-member-to-channel-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-member-to-channel-dialog.component.html',
+  styleUrl: './add-member-to-channel-dialog.component.scss'
+})
+export class AddMemberToChannelDialogComponent {
+
+}
