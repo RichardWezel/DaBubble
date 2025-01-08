fe4226245ac4600fcf37a876a5e0dd27e0a53287
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-01-08 20:08:26 +0100

    JSDocs: new-message-input-head

--- src/app/components/workspace/channel/channel-head/new-message-input-head/new-message-input-head.component.ts
+++ src/app/components/workspace/channel/channel-head/new-message-input-head/new-message-input-head.component.ts
@@ -20,15 +20,17 @@ export class NewMessageInputHeadComponent {
   userInput: string = ''; // Binding to input value
   suggestion: string = ''; // saves the current autocomplete
 
+
   /**
-   * During input in the input field, the function “updateSuggestion” is called, which updates the suggested text in the background of input field.
+   * Handles the input change event and updates the autocomplete suggestion.
    */
   onInput(): void {
     this.updateSuggestion();
   }
 
+
   /**
-   * Finds a conditional match with usern, channel or e-mail and sets the default text for autocomplete.
+   * Updates the autocomplete suggestions based on the user input.
    */
   updateSuggestion(): void {
     if (this.userInput) {
@@ -39,11 +41,11 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
   /**
-   * Depending on the initial prefix, a function is called that returns a match with the entered term from the respective property array.
-   * 
-   * @param userInput - Input String of User
-   * @returns 
+   * Searches for a match in channels, users, or emails based on the prefix and user input.
+   * @param userInput - The input string from the user.
+   * @returns A matching string or undefined if no match is found.
    */
   findMatch(userInput: string): string | undefined {
     let prefix = userInput.slice(0, 1);
@@ -59,12 +61,11 @@ export class NewMessageInputHeadComponent {
     return undefined;
   }
 
+
   /**
-   * If userInput starts with a #, the first channel name of the current user's channel is returned. 
-   * If further inputs follow #, matching channel names of the current user are returned.  
-   * 
-   * @param userInput - Input String of User
-   * @returns 
+   * Searches for channels based on the user input.
+   * @param userInput - The input string including the '#' prefix.
+   * @returns The matched channel name or undefined if no match is found.
    */
   handleChannelSearch(userInput: string): string | undefined {
     let inputHasOnlyPrefix = userInput.length === 1;
@@ -80,12 +81,11 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
   /**
-   * If userInput starts with a @, the first User name is returned. 
-   * If further inputs follow @, matching User names returned.  
-   * 
-   * @param userInput - Input String of User
-   * @returns 
+   * Searches for users based on the user input.
+   * @param userInput - The input string including the '@' prefix.
+   * @returns The matched user name or undefined if no match is found.
    */
   handleUserSearch(userInput: string): string | undefined {
     if (userInput.length === 1) {
@@ -96,15 +96,20 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+  /**
+   * Searches for emails matching the user input.
+   * @param userInput - The user input string.
+   * @returns The matched email or undefined if no match is found.
+   */
   handleEmailSearch(userInput: string): string | undefined {
     return this.matchEmail(userInput);
   }
 
+
   /**
-   * Finds the first match between the input content and the CurrentUserChannel array and return an object of matched channel.
-   * 
-   * @param searchTerm - Input String of User
-   * @returns - object of channel is matched
+   * Finds the first matching channel name from the storage based on the search term.
+   * @param searchTerm - The search term provided by the user.
+   * @returns The name of the matched channel or undefined.
    */
   matchChannel(searchTerm: string): string | undefined {
     let channels: ChannelInterface[] = this.storage.CurrentUserChannel;
@@ -114,11 +119,11 @@ export class NewMessageInputHeadComponent {
     return match?.name;
   }
 
+
   /**
-   * Finds the first match between the input content and the User array and returns an object of matched channel.
-   * 
-   * @param searchTerm 
-   * @returns 
+   * Finds the first matching user name from the storage based on the search term.
+   * @param searchTerm - The search term provided by the user.
+   * @returns The name of the matched user or undefined.
    */
   matchUser(searchTerm: string): string | undefined {
     let users: UserInterface[] = this.storage.user;
@@ -128,6 +133,11 @@ export class NewMessageInputHeadComponent {
     return match?.name;
   }
 
+  /**
+   * Finds the first matching email from the storage based on the search term.
+   * @param searchTerm - The search term provided by the user.
+   * @returns The email of the matched user or undefined.
+   */
   matchEmail(searchTerm: string): string | undefined {
     let users: UserInterface[] = this.storage.user;
     let match = users.find(user =>
@@ -136,9 +146,10 @@ export class NewMessageInputHeadComponent {
     return match?.email;
   }
 
+
   /**
-   * A getter that assembles the displayed text in the suggestion div. 
-   * It combines the userInput with the rest of the suggestion to display the suggestion.
+   * Constructs the display text for the suggestion box by combining user input with the suggestion.
+   * @returns The full text to be displayed in the suggestion box.
    */
   get displayText(): string {
     if (!this.suggestion) {
@@ -146,6 +157,7 @@ export class NewMessageInputHeadComponent {
     }
     return this.handleUserInputForSuggestionText();
   }
+
 
   handleUserInputForSuggestionText() {
     const prefix = this.userInput.charAt(0);
@@ -162,6 +174,13 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
+  /**
+   * Combines the user input with the remaining part of the suggestion to complete the input string.
+   * This method is used after a prefix is recognized to append the rest of a partially typed suggestion.
+   *
+   * @returns {string} The complete string combining the user input with the remaining term from the suggestion.
+   */
   returnUserInputWithRemainingTerm() {
     const searchTerm = this.userInput.slice(1);
     const remainingTerm = this.suggestion.slice(searchTerm.length);
@@ -171,11 +190,11 @@ export class NewMessageInputHeadComponent {
 
   // FUNCTIONS FOR TRANSFERRING THE SUGGESTION TO THE CHANNEL DISPLAY
 
+
   /**
-     * Renders the userInput such as channel or user by pressing Enter or Tab key.
-     * 
-     * @param event 
-     */
+   * Accepts and renders user input for channels or users by handling the Tab or Enter key events.
+   * @param event - The keyboard event triggered by the user.
+   */
   onKeyDown(event: KeyboardEvent): void {
     if (event.key === 'Tab' && this.suggestion ||
       event.key === 'Enter' && this.suggestion) {
@@ -183,11 +202,10 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
   /**
-   * Accepts the submitted autocomplete by transferring it to the input field. 
-   * Calls the showSuggestion() function, which renders the channel or direct messages.
-   * 
-   * @param event 
+   * Submits the selected autocomplete suggestion, updates the input field, and performs the display action.
+   * @param event - The keyboard event that triggered the submit.
    */
   handleSubmitSuggestion(event: KeyboardEvent) {
     event.preventDefault();
@@ -195,9 +213,10 @@ export class NewMessageInputHeadComponent {
     this.showSuggestion();
   }
 
+
   /**
-   * Depending on which prefix is placed in front, 
-   * the submitted autocomplete with the corresponding prefix is placed as content in the input field.
+   * Accepts the autocomplete suggestion based on the prefix and updates the input field accordingly.
+   * Prefixes include '#' for channels and '@' for user mentions.
    */
   acceptSuggestion(): void {
     if (this.userInput.startsWith('#')) {
@@ -212,10 +231,10 @@ export class NewMessageInputHeadComponent {
     console.log('acceptSuggestion(), suggestion: ', this.suggestion);
   }
 
+
   /**
-   * Depending on whether # or @ is at the beginning of the input field, 
-   * the channel of the currentUser is set to the entered channel or direct message 
-   * so that it is displayed.
+   * Displays the autocomplete suggestion by setting the appropriate channel or direct message.
+   * Calls specific methods based on whether the input starts with '#' (channel) or '@' (direct message).
    */
   showSuggestion() {
     let prefix = this.userInput.charAt(0);
@@ -231,22 +250,20 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
   /**
-   * Sets the current Channel of current User to the submitted channel
-   * 
-   * @param {string} searchTerm  - channel name as submitted
+   * Sets the active channel to the one submitted by the user.
+   * @param {string} searchTerm - The name of the channel as submitted by the user.
    */
   showSubmittedChannel(searchTerm: string) {
     let foundChannelId = this.findChannelId(searchTerm);
     this.navigationService.setChannel(foundChannelId);
   }
 
+
   /**
-   * Sets the current Channel of current User to the submitted direct message user. 
-   * If a dm already exists, it will be shown, 
-   * otherwise a new dm will be created for the affected users (current user and submitted user) and shown.
-   * 
-   * @param {string} searchTerm - user name as submitted
+   * Displays an existing or creates a new direct message based on the submitted user name.
+   * @param {string} searchTerm - The name of the user as submitted by the user.
    */
   showSubmittedDirectMessage(searchTerm: string) {
     const userOfSuggestion = this.storage.user.find(user => user.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
@@ -258,21 +275,34 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
+  /**
+   * Displays an existing direct message session if available.
+   * @param {UserInterface} userOfSuggestion - The user with whom the direct message session exists.
+   */
   showExistingDm(userOfSuggestion: UserInterface) {
     let dmsOfCurrentUser = this.storage.user.find(user => user.id === this.storage.currentUser.id)?.dm;
     let dmWithUserOfSuggestion = dmsOfCurrentUser?.find(dm => dm.contact === userOfSuggestion.id);
     this.navigationService.setChannel(dmWithUserOfSuggestion!.id);
   }
 
+
+  /**
+   * Creates and displays a new direct message session with the suggested user.
+   * @param {UserInterface} userOfSuggestion - The user to create a new direct message session with.
+   */
   async showNewDm(userOfSuggestion: UserInterface) {
     await this.createEmptyDms(userOfSuggestion);
     let dmWithUserOfSuggestion = this.storage.user.find(user => user.id === this.storage.currentUser.id)?.dm.find(dm => dm.contact === userOfSuggestion.id);
     if (dmWithUserOfSuggestion) this.navigationService.setChannel(dmWithUserOfSuggestion!.id);
   }
 
+
+  /**
+   * Displays an email as a direct message if applicable.
+   */
   showSubmittedEmail() {
     const userOfSuggestion = this.storage.user.find(user => user.email.startsWith(this.userInput.toLowerCase()));
-
     if (userOfSuggestion && this.findUserInDms(userOfSuggestion)) {
       this.showExistingDmEmail(userOfSuggestion)
     } else if (userOfSuggestion && !this.findUserInDms(userOfSuggestion)) {
@@ -280,12 +310,22 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
+  /**
+   * Displays an existing email direct message session if available.
+   * @param {UserInterface} userOfSuggestion - The user with whom the direct message session exists based on email.
+   */
   showExistingDmEmail(userOfSuggestion: UserInterface) {
     let dmsOfCurrentUser = this.storage.user.find(user => user.id === this.storage.currentUser.id)?.dm;
     let dmWithUserOfSuggestion = dmsOfCurrentUser?.find(dm => dm.contact === userOfSuggestion.id);
     this.navigationService.setChannel(dmWithUserOfSuggestion!.id);
   }
 
+
+  /**
+   * Creates and displays a new direct message session with the suggested user based on email.
+   * @param {UserInterface} userOfSuggestion - The user to create a new direct message session with based on email.
+   */
   async showNewDmEmail(userOfSuggestion: UserInterface) {
     console.log('showNewDmEmail(userOfSuggestion: UserInterface): ', userOfSuggestion)
     await this.createEmptyDms(userOfSuggestion);
@@ -293,6 +333,11 @@ export class NewMessageInputHeadComponent {
     if (dmWithUserOfSuggestion) this.navigationService.setChannel(dmWithUserOfSuggestion!.id);
   }
 
+
+  /**
+   * Creates empty direct message entries for initiating contact between the current user and the suggested user.
+   * @param {UserInterface} userOfSuggestion - The user to initiate a direct message session with.
+   */
   async createEmptyDms(userOfSuggestion: UserInterface) {
     let currentUserId = this.storage.currentUser.id;
     let suggestedUserId = userOfSuggestion.id;
@@ -302,22 +347,34 @@ export class NewMessageInputHeadComponent {
     }
   }
 
+
+  /**
+   * Checks if there is an existing direct message session with the suggested user.
+   * @param {UserInterface} userOfSuggestion - The user to check for an existing direct message session.
+   * @returns {boolean} True if a direct message session exists, otherwise false.
+   */
   findUserInDms(userOfSuggestion: UserInterface): boolean {
     let match = this.storage.user.find(user => user.id === this.storage.currentUser.id)?.dm.some(dm => dm.contact === userOfSuggestion.id);
     if (match) return true;
     else return false;
   }
 
+
+  /**
+   * Searches for an existing direct message session within the current user's direct messages with a specified user.
+   * @param {UserInterface} foundUser - The user to search for within the current user's direct messages.
+   * @returns {Object} The direct message session if found, otherwise undefined.
+   */
   findUserInCurrentUserDms(foundUser: UserInterface) {
     let match = this.storage.user.find(user => user.id === this.storage.currentUser.id)?.dm.find(user => user.id === foundUser.id);
     return match
   }
 
+
   /**
    * Matches the input with the names of the channels of the current user and returns the id of the matching channel.
-   * 
-   * @param searchTerm - string of input content
-   * @returns - id of channel is matched with input
+   * @param {string} searchTerm - String of input content used to match channel names.
+   * @returns {string} The id of the channel that matches the input, if any.
    */
   findChannelId(searchTerm: string): string {
     let channels: ChannelInterface[] = this.storage.CurrentUserChannel;
@@ -326,15 +383,24 @@ export class NewMessageInputHeadComponent {
     return match?.id!;
   }
 
+
+  /**
+   * Retrieves the name of the channel that the current user is currently in.
+   * @returns {string} The name of the current channel, if any.
+   */
   channelName() {
     return this.storage.channel.find(channel => channel.id === this.storage.currentUser.currentChannel)?.name;
   }
 
+
+  /**
+   * Retrieves the list of users associated with the current user's active channel.
+   * @returns {UserInterface[]} A list of users in the current channel, or an empty array if no users are found.
+   */
   channelUser() {
     let users = this.storage.channel.find(channel => channel.id === this.storage.currentUser.currentChannel)?.user;
     if (users) return users;
     else return [];
   }
-
 
 }
