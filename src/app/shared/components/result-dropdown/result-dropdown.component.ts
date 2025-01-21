d84a1c0df3500d82aab753e4f01d294d596f56bb
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-01-21 18:42:28 +0100

    created new componnet result-dropdown

---
+++ src/app/shared/components/result-dropdown/result-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './result-dropdown.component.html',
+  styleUrl: './result-dropdown.component.scss'
+})
+export class ResultDropdownComponent {
+
+}
