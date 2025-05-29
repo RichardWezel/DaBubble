5f7a540fd17f552c5223ef5f7bdc90eda424f9d6
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-05-29 11:51:43 +0200

    node package reinstalled

--- src/app/components/workspace/thread/thread-messages/thread-messages.component.ts
+++ src/app/components/workspace/thread/thread-messages/thread-messages.component.ts
@@ -6,7 +6,7 @@ import { MessageComponent } from '../../../../shared/components/message/message.
 @Component({
   selector: 'app-thread-messages',
   standalone: true,
-  imports: [DateSeparatorComponent, MessageComponent],
+  imports: [DateSeparatorComponent, MessageComponent, ChannelMessagesComponent],
   templateUrl: './thread-messages.component.html',
   styleUrl: './thread-messages.component.scss'
 })
